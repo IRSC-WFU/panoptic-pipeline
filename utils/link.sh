fd6b92a74439a30af883cf5eea916d5db929b3bc
Robert Langefeld <langrc18@wfu.edu>
2021-06-16 12:54:32 -0400

    Fixed paths in link.sh

--- utils/link.sh
+++ utils/link.sh
@@ -25,32 +25,32 @@ mkdir -p $COCO/images
 ################
 
 # Unlink Annotation Files
-rm $COCO/annotations/coco_train.json
-rm $COCO/annotations/coco_val.json
-rm $COCO/annotations/coco_test.json
+rm $COCO/annotations/coco_train.json 2>/dev/null
+rm $COCO/annotations/coco_val.json 2>/dev/null
+rm $COCO/annotations/coco_test.json 2>/dev/null
 
 # Unlink Panoptic Files
-rm $COCO/annotations/panoptic_train2017.json
-rm $COCO/annotations/panoptic_val2017.json
-rm $COCO/annotations/panoptic_test2017.json
+rm $COCO/annotations/panoptic_train2017.json 2>/dev/null
+rm $COCO/annotations/panoptic_val2017.json 2>/dev/null
+rm $COCO/annotations/panoptic_test2017.json 2>/dev/null
 
 # Unlink Instance Files
-rm $COCO/annotations/instances_train2017.json
-rm $COCO/annotations/instances_val2017.json
-rm $COCO/annotations/instances_test2017.json
+rm $COCO/annotations/instances_train2017.json 2>/dev/null
+rm $COCO/annotations/instances_val2017.json 2>/dev/null
+rm $COCO/annotations/instances_test2017.json 2>/dev/null
 
 # Unlink Image Directories
-rm $COCO/images/train2017
-rm $COCO/images/val2017
-rm $COCO/images/test2017
+rm $COCO/images/train2017 2>/dev/null
+rm $COCO/images/val2017 2>/dev/null
+rm $COCO/images/test2017 2>/dev/null
 
 # Unlink Panoptic Directories
-rm $COCO/annotations/panoptic_train2017
-rm $COCO/annotations/panoptic_val2017
-rm $COCO/annotations/panoptic_test2017
+rm $COCO/annotations/panoptic_train2017 2>/dev/null
+rm $COCO/annotations/panoptic_val2017 2>/dev/null
+rm $COCO/annotations/panoptic_test2017 2>/dev/null
 
 # Unlink Categories File
-rm $COCO/annotations/panoptic_coco_categories.json
+rm $COCO/annotations/panoptic_coco_categories.json 2>/dev/null
 
 ##############
 # Link Files #
@@ -61,27 +61,27 @@ rm $COCO/annotations/panoptic_coco_categories.json
 # ln -s $BASE/coco_val.json $COCO/annotations/coco_val.json
 # ln -s $BASE/coco_test.json $COCO/annotations/coco_test.json
 
-mv $BASE/coco_train.json $COCO/annotations/coco_train.json
-mv $BASE/coco_val.json $COCO/annotations/coco_val.json
-mv $BASE/coco_test.json $COCO/annotations/coco_test.json
+mv $BASE/data/train/coco_train.json $COCO/annotations/coco_train.json
+mv $BASE/data/val/coco_val.json $COCO/annotations/coco_val.json
+mv $BASE/data/test/coco_test.json $COCO/annotations/coco_test.json
 
 # Link Panoptic Files
 # ln -s $BASE/panoptic_train.json $COCO/annotations/panoptic_train2017.json
 # ln -s $BASE/panoptic_val.json $COCO/annotations/panoptic_val2017.json
 # ln -s $BASE/panoptic_test.json $COCO/annotations/panoptic_test2017.json
 
-mv $BASE/panoptic_train.json $COCO/annotations/panoptic_train2017.json
-mv $BASE/panoptic_val.json $COCO/annotations/panoptic_val2017.json
-mv $BASE/panoptic_test.json $COCO/annotations/panoptic_test2017.json
+mv $BASE/data/train/panoptic_train.json $COCO/annotations/panoptic_train2017.json
+mv $BASE/data/val/panoptic_val.json $COCO/annotations/panoptic_val2017.json
+mv $BASE/data/test/panoptic_test.json $COCO/annotations/panoptic_test2017.json
 
 # Link Instance Files
 # ln -s $BASE/instances_train.json $COCO/annotations/instances_train2017.json
 # ln -s $BASE/instances_val.json $COCO/annotations/instances_val2017.json
 # ln -s $BASE/instances_test.json $COCO/annotations/instances_test2017.json
 
-mv $BASE/instances_train.json $COCO/annotations/instances_train2017.json
-mv $BASE/instances_val.json $COCO/annotations/instances_val2017.json
-mv $BASE/instances_test.json $COCO/annotations/instances_test2017.json
+mv $BASE/data/train/instances_train.json $COCO/annotations/instances_train2017.json
+mv $BASE/data/val/instances_val.json $COCO/annotations/instances_val2017.json
+mv $BASE/data/test/instances_test.json $COCO/annotations/instances_test2017.json
 
 # Link Image Directories
 ln -s $BASE/data/train/images $COCO/images/train2017
